523ad6283fb92d2e7be3e0a33ce12d1c5c79698d
Nextech <ryansam@shortridgehigh.org>
2017-06-28 09:27:18 -0400

    Friends list page and some portfolio updates

---
+++ JavaScriptbasic/0.10_popquiz.js
@@ -0,0 +1,70 @@
+var splitter = "----------------------------------------------------------"
+
+//1
+
+for (var i = 0; i < 101; i += 10) {
+	if (i == 60) {
+		console.log("Sixty");
+	}else if (i !== 60){
+		console.log(i)
+		}
+	};
+console.log(splitter + 2)
+
+//2 
+
+ var Friends = ['Luke' ,'Henry', 'Austin', 'Marshal']
+
+for (var i = 0; i < Friends.length; i++) {
+	Friends[i]
+
+	console.log([Friends[i]])
+}
+
+
+console.log(splitter + 3)
+
+//3 
+
+function area(height,width) {
+	area = (height*width)
+	console.log(area)
+} 
+
+//// OR function product(l*W), console.log (product)
+
+area(5,5);
+
+
+//4
+
+console.log(splitter + 4)
+
+
+for (var i = 0; i < 50; i ++) {
+
+	if (i % 3 == 0 && i % 2 == 0 ){  //do most unlikely/most variables to likely/less
+		console.log("MizzMuzz");
+
+	} else if (i % 2 === 0) {
+		console.log("Mizz");
+
+	}else if (i % 3 === 0){
+		console.log("Muzz")
+	}else {
+		console.log(i)
+	}
+} 
+
+//5 
+
+console.log(splitter + 5)
+
+var languages = ['python','C#','HTML','Java']
+	console.log(languages[2]);
+ 
+
+//6
+
+console.log(splitter + 6)
+
